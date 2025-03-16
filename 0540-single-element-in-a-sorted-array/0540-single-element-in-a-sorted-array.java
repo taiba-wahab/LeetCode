5f67b794f05fe7513f2314e1de6a7fc284019651
taiba-wahab <taibawahab5@gmail.com>
2025-03-16 23:35:09 +0530

    Time: 4 ms (5.49%), Space: 50.2 MB (66.12%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,27 @@
 class Solution {
+    public int search(int [] nums, int low, int high, int key) {
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == key) {
+                return 1;
+            }
+            else if(nums[mid] > key) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return 0;
+    }
     public int singleNonDuplicate(int[] nums) {
         int size = nums.length;
         int answer = 0;
-        for(int i = 0; i < size; i++) {
-            answer ^= nums[i];
+        for(int i = 0; i < size; i= i + 2) {
+            if(search(nums, i + 1, size - 1, nums[i]) == 0) {
+                answer = nums[i];
+                break;
+            }
         }
         return answer;
     }

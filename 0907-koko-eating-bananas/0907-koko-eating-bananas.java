7201bf34ec522675267ce879341adec2b2f79d80
taiba-wahab <taibawahab5@gmail.com>
2025-03-16 13:06:51 +0530

    Time: 13 ms (65.8%), Space: 45.5 MB (15.8%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,18 +1,18 @@
 class Solution {
     boolean isPossible(int [] piles, int h, int rate) {
         long hours = 0;
-        for(int k = 0; k < piles.length; k++) {
-            if(piles[k] % rate != 0) {
+        for(int i = 0; i < piles.length; i++) {
+            if(piles[i] % rate != 0) {
                 hours += 1;
             }
-            hours += piles[k] / rate;
+            hours += piles[i] / rate;
         }
         return hours <= h;
     }
     public int minEatingSpeed(int[] piles, int h) {
+        int answer = 0;
         int start = 1;
         int end = Arrays.stream(piles).max().getAsInt();
-        int answer = 0;
         while(start <= end) {
             int mid = start + (end - start) / 2;
             boolean possible = isPossible(piles, h, mid);

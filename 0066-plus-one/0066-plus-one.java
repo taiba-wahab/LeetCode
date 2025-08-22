0ca33556c26500acf4c78bbb026132ac365400ee
taiba-wahab <taibawahab5@gmail.com>
2025-08-22 23:00:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.41%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for(int i = length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] answer = new int[length + 1];
+        answer[0] = 1;
+        return answer;
+    }
+}

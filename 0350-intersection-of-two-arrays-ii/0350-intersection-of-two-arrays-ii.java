fbd28546b0b3ff18813528fd3fcb3749ddc18c64
taiba-wahab <taibawahab5@gmail.com>
2025-08-23 21:28:38 +0530

    Time: 3 ms (91.63%), Space: 43.1 MB (77.13%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        // int size = nums1.length > nums2.length? nums2.length : nums1.length;
-        ArrayList<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        for(int i = 0; i < nums1.length; i++) {
-            map1.put(nums1[i], map1.getOrDefault(nums1[i],0) + 1);
-        }
-        for(int i = 0; i < nums2.length; i++) {
-            map2.put(nums2[i], map2.getOrDefault(nums2[i],0) + 1);
-        }
-        for(Integer key : map1.keySet()) {
-            if(map2.containsKey(key)) {
-                int times = map1.get(key) > map2.get(key) ? map2.get(key) : map1.get(key);
-                for(int i = 1; i <= times; i++) {
-                    ans.add(key);
-                }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int [] answer = new int[nums1.length > nums2.length? nums2.length : nums1.length];
+        int i = 0, j = 0, idx = 0;
+        while(i < nums1.length && j < nums2.length) {
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]) {
+                j++;
+            }
+            else {
+                answer[idx] = nums1[i];
+                i++;
+                j++;
+                idx++;
             }
         }
-        int[] result = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i); 
-        }
-        return result;
+        return Arrays.copyOfRange(answer, 0, idx); 
     }
 }

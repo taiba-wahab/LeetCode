8349dfc2a65d7f232f19ddcf65b312a90e89a067
taiba-wahab <taibawahab5@gmail.com>
2025-08-24 23:04:31 +0530

    Time: 3 ms (97.18%), Space: 44.4 MB (71.42%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -9,18 +9,31 @@
  * }
  */
 class Solution {
+    public int getSize(ListNode head) {
+        ListNode temp = head;
+        int size = 0;
+        while(temp != null) {
+            size++;
+            temp = temp.next;
+        }
+        return size;
+    }
     public ListNode insertionSortList(ListNode head) {
+        int size = getSize(head);
+        int[] list = new int[size];
+        ListNode temp = head;
+        int idx = 0;
+        while(temp != null) {
+            list[idx] = temp.val;
+            temp = temp.next;
+            idx++;
+        }
+        Arrays.sort(list);
         ListNode dummy = new ListNode(0);
-        ListNode curr = head;
-        while(curr != null) {
-            ListNode next = curr.next;
-            ListNode prev = dummy;
-            while(prev.next != null && prev.next.val < curr.val) {
-                prev = prev.next;
-            }
-            curr.next = prev.next;
-            prev.next = curr;
-            curr = next;
+        ListNode result = dummy;
+        for(int i = 0; i < size; i++) {
+            result.next = new ListNode(list[i]);
+            result = result.next;
         }
         return dummy.next;
     }

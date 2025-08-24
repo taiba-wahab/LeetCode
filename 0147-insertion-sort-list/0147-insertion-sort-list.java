24dc63b91cc4e506728d7036275c2fb46cd9ebc7
taiba-wahab <taibawahab5@gmail.com>
2025-08-24 23:13:52 +0530

    Time: 21 ms (10.47%), Space: 44.5 MB (56.85%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -9,43 +9,18 @@
  * }
  */
 class Solution {
-    public void insertionSort(int arr[]) {
-        int n = arr.length;
-        for(int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while(j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-        arr[j + 1] = key;
-        }
-    }
-    public int getSize(ListNode head) {
-        ListNode temp = head;
-        int size = 0;
-        while(temp != null) {
-            size++;
-            temp = temp.next;
-        }
-        return size;
-    }
     public ListNode insertionSortList(ListNode head) {
-        int size = getSize(head);
-        int[] list = new int[size];
-        ListNode temp = head;
-        int idx = 0;
-        while(temp != null) {
-            list[idx] = temp.val;
-            temp = temp.next;
-            idx++;
-        }
-        insertionSort(list);
         ListNode dummy = new ListNode(0);
-        ListNode result = dummy;
-        for(int i = 0; i < size; i++) {
-            result.next = new ListNode(list[i]);
-            result = result.next;
+        ListNode curr = head;
+        while(curr != null) {
+            ListNode next = curr.next;
+            ListNode prev = dummy;
+            while(prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+            curr.next = prev.next;
+            prev.next = curr;
+            curr = next;
         }
         return dummy.next;
     }

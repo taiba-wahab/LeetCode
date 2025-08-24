d3a4d2e212f2a18ce8d929c2223bda80953c93f4
taiba-wahab <taibawahab5@gmail.com>
2025-08-24 23:13:35 +0530

    Time: 15 ms (70.47%), Space: 44.4 MB (85.58%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -9,6 +9,18 @@
  * }
  */
 class Solution {
+    public void insertionSort(int arr[]) {
+        int n = arr.length;
+        for(int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+        arr[j + 1] = key;
+        }
+    }
     public int getSize(ListNode head) {
         ListNode temp = head;
         int size = 0;
@@ -28,7 +40,7 @@ class Solution {
             temp = temp.next;
             idx++;
         }
-        Arrays.sort(list);
+        insertionSort(list);
         ListNode dummy = new ListNode(0);
         ListNode result = dummy;
         for(int i = 0; i < size; i++) {

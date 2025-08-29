d8a85a929acedebc12997ec630b21123cd6a57ca
taiba-wahab <taibawahab5@gmail.com>
2025-08-29 22:39:14 +0530

    Time: 0 ms (100%), Space: 40.3 MB (100%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -1,24 +1,10 @@
 class Solution {
-    public long countOdds(int n) {
-        long odds = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 != 0) odds++; 
-        }
-        return odds;
-    }
-    public long countEvens(int m) {
-        long evens = 0;
-        for(int i = 1; i <= m; i++) {
-            if(i % 2 == 0) evens++; 
-        }
-        return evens;
-    }
     public long flowerGame(int n, int m) {
-        long oddInN = countOdds(n);
-        long oddInM = countOdds(m);
-        long evenInN = countEvens(n);
-        long evenInM = countEvens(m);
-        long answer = (oddInN * evenInM) + (oddInM * evenInN);
-        return answer;
+        long oddInN = (n + 1) / 2;
+        long evenInN = n / 2;
+        long oddInM = (m + 1) / 2;
+        long evenInM = m / 2;
+
+        return oddInN * evenInM + evenInN * oddInM;
     }
 }

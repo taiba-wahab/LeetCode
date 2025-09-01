5cf3c41144f2dda903f0520d64e399c960fd4141
taiba-wahab <taibawahab5@gmail.com>
2025-09-01 16:01:50 +0530

    Time: 23 ms (87.66%), Space: 43.8 MB (9.8%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        int n = nums.length;
+        for(int k = n - 1; k >= 0; k--) {
+            int i = 0, j = k - 1;
+            while(i < j) {
+                if(nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    j--;
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}

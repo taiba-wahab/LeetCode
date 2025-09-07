6484e8685d424a116020a5e3673af53e3195c5cd
taiba-wahab <taibawahab5@gmail.com>
2025-09-07 14:37:08 +0530

    Time: 1 ms (99.85%), Space: 46.6 MB (10.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int answer = 0;
+        int ans = 0;
         for(int i = 0; i < nums.length; i++) {
-            answer ^= nums[i];
+            ans ^= nums[i];
         }
-       return answer;
+        return ans;
     }
 }

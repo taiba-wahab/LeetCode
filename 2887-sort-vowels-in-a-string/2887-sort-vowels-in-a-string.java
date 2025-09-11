050896b59f3e0cc36a46a88ba5d1bcf77e679d66
taiba-wahab <taibawahab5@gmail.com>
2025-09-11 20:52:08 +0530

    Time: 26 ms (81.67%), Space: 45.7 MB (94.75%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,35 +1,27 @@
 class Solution {
     public String sortVowels(String s) {
-        char[] vowels = new char[10];
-        for(int i = 0; i < 10; i++) { //O(1)
-            vowels[0] = 'A';
-            vowels[1] = 'E';
-            vowels[2] = 'I';
-            vowels[3] = 'O';
-            vowels[4] = 'U';
-            vowels[5] = 'a';
-            vowels[6] = 'e';
-            vowels[7] = 'i';
-            vowels[8] = 'o';
-            vowels[9] = 'u';
-        }
-        ArrayList<Character> vowelsList = new ArrayList<>();
+        String vowels = "AEIOUaeiou";
         int n = s.length();
+        int[] freq = new int[128];
         for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            boolean found = new String(vowels).chars().anyMatch(c -> c== ch);
-            if(found) {
-                vowelsList.add(ch);
+            if(vowels.indexOf(ch) != -1) {
+                freq[ch]++;
             }
         }
-        Collections.sort(vowelsList);
-        Set<Character> vowelSet = new HashSet<>(vowelsList);
+        StringBuilder sortedVowels = new StringBuilder();
+        for(int i = 0; i < 128; i++) {
+            while(freq[i] > 0) {
+                sortedVowels.append((char)i);
+                freq[i]--;
+            }
+        }
         StringBuilder ans = new StringBuilder();
         int idx = 0;
         for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(vowelSet.contains(ch)) {
-                ans.append(vowelsList.get(idx));
+            if(vowels.indexOf(ch) != -1) {
+                ans.append(sortedVowels.charAt(idx));
                 idx++;
             }
             else {

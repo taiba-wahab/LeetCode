154283833da83742c9333fa304658300cf79a8ad
taiba-wahab <taibawahab5@gmail.com>
2025-09-12 22:41:56 +0530

    Time: 3 ms (96.06%), Space: 45.6 MB (66.93%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int countVowels = 0;
         int n = s.length();
         for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u') {
-                countVowels++;
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
             }
-        }
-        if(countVowels > 0) {
-            return true;
         }
         return false;
     }

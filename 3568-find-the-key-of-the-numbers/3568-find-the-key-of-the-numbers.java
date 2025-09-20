21b6eb7cba0cafba0a53fd0556aa42c31063259b
taiba-wahab <taibawahab5@gmail.com>
2025-09-20 16:15:38 +0530

    Time: 14 ms (21.2%), Space: 43 MB (12.05%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.java
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.java
@@ -1,33 +1,9 @@
 class Solution {
-    public int countDigits(int num) {
-        int count = 0;
-        while(num != 0) {
-            int rem = num % 10;
-            count++;
-            num /= 10;
-        }
-        return count;
-    }
-    public String padZeroes(int num) {
-        String n = String.valueOf(num);
-        StringBuilder sb = new StringBuilder();
-        int digits = countDigits(num);
-        int countOfZeroes = 0;
-        while(countOfZeroes != (4 - digits)) {
-            sb.append("0");
-            countOfZeroes++;
-        }
-        sb.append(String.valueOf(num));
-        return sb.toString();
-    }
-    public int findMinimum(int[] array) {
-        return Arrays.stream(array).min().getAsInt();
-    }
     public int generateKey(int num1, int num2, int num3) {
         StringBuilder sb = new StringBuilder();
-        String a = padZeroes(num1);
-        String b = padZeroes(num2);
-        String c = padZeroes(num3);
+        String a = String.format("%04d", num1);
+        String b = String.format("%04d", num2);
+        String c = String.format("%04d", num3);
         for(int i = 0; i < 4; i++) {
             int min = Math.min(a.charAt(i) - '0', Math.min(b.charAt(i) - '0', c.charAt(i) - '0'));
             sb.append(String.valueOf(min));

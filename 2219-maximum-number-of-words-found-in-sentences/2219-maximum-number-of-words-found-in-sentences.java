aec55fea9eceaa04c384e4b52a65ea360c0090a2
taiba-wahab <taibawahab5@gmail.com>
2025-11-17 23:03:39 +0530

    Time: 4 ms (23.1%), Space: 44.3 MB (40.81%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int getMaxLength(String s) {
-        String[] words = s.split(" ");
-        return words.length;
-    }
     public int mostWordsFound(String[] sentences) {
         int maxLength = 0;
         for(int i = 0; i < sentences.length; i++) {
-            int length = getMaxLength(sentences[i]);
-            if(length > maxLength) maxLength = length;
+            String s = sentences[i];
+            int count = 1;
+            for(int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) == ' ') count++;
+            }
+            maxLength = Math.max(count, maxLength);
         }
         return maxLength;
     }

4c320591ed2eb2cfe831959876a9ef7bf185186c
taiba-wahab <taibawahab5@gmail.com>
2025-11-17 15:59:49 +0530

    Time: 2 ms (78.16%), Space: 45.5 MB (6.01%) - LeetHub

---
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        boolean missing = false;
+        int j = 1;
+        while(!missing) {
+            if(set.contains(j * k)) {
+                j++;
+            }
+            else {
+                //j++;
+                missing = true;
+            }
+        }
+        return j * k;
+    }
+}

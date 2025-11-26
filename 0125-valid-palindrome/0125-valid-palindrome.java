dd8c1c7dd9a8a157847b6da9ca83a62bba410d3b
taiba-wahab <taibawahab5@gmail.com>
2025-11-26 15:46:54 +0530

    Time: 2 ms (98.66%), Space: 44.3 MB (54.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
         s = s.toLowerCase();
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if((ch >= 'a' && ch <= 'z') || (ch >= 48 && ch <= 57)) {
-                sb.append(ch);
-            }
-        }
         int i = 0;
-        int j = sb.length() - 1;
+        int j = s.length() - 1;
         while(i < j) {
-            if(sb.charAt(i) != sb.charAt(j)) return false;
-            i++;
-            j--;
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if(!(ch1 >= 'a' && ch1 <= 'z') && !(ch1 >= 48 && ch1 <= 57)) {
+                i++;
+                continue;
+            }
+            if(!(ch2 >= 'a' && ch2 <= 'z') && !(ch2 >= 48 && ch2 <= 57)) {
+                j--;
+                continue;
+            }
+            if(ch1 != ch2) return false;
+            else {
+                i++;
+                j--;
+            }
         }
         return true;
     }

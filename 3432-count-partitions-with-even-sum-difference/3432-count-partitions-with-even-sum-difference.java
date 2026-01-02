df173fe43e159f41da191fbe9cf7d280504264f1
taiba-wahab <taibawahab5@gmail.com>
2026-01-02 22:39:07 +0530

    Time: 4 ms (12.79%), Space: 44.2 MB (5.74%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -1,27 +1,7 @@
 class Solution {
-    public int leftSum(int[] nums, int idx) {
-        int left = 0;
-        for(int i = 0; i <= idx; i++) {
-            left += nums[i];
-        }
-        return left;
-    }
-    public int rightSum(int[] nums, int idx) {
-        int right = 0;
-        for(int i = idx + 1; i < nums.length; i++) {
-            right += nums[i];
-        }
-        return right;
-    }
     public int countPartitions(int[] nums) {
-        int count = 0;
-        for(int i = 0; i < nums.length - 1; i++) {
-            int left = leftSum(nums, i);
-            int right = rightSum(nums, i);
-            if((left - right) % 2 == 0) {
-                count++;
-            }
-        }
-        return count;
+        int totalSum = Arrays.stream(nums).sum();
+        if(totalSum % 2 == 0) return nums.length - 1;
+        return 0;
     }
 }

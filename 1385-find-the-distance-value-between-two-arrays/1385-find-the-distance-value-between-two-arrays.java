cf9c90acc3d55fc3af46102a1a801d740d3a3432
taiba-wahab <taibawahab5@gmail.com>
2026-02-03 01:34:04 +0530

    Time: 3 ms (97.92%), Space: 46.3 MB (77.4%) - LeetHub

---
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int count = 0;
+        for(int i = 0; i < arr1.length; i++) {
+            int flag = 0;
+            for(int j = 0; j < arr2.length; j++) {
+                if(Math.abs(arr1[i] - arr2[j]) <= d) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag != 1) count++;
+        }
+        return count;
+    }
+}

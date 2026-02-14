3a42d7fe2c33d9e4142a71cc06655e2e70df4a25
taiba-wahab <taibawahab5@gmail.com>
2026-02-14 18:28:55 +0530

    Time: 3 ms (97.57%), Space: 46.6 MB (15.59%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,21 +14,24 @@
  * }
  */
 class Solution {
-    public int validPaths(TreeNode root, long target) {
-        int count = 0;
-        if(root == null) return 0;
-        if(root.val == target) count++;
-        target -= root.val;
-        count += validPaths(root.left, target);
-        count += validPaths(root.right, target);
-        return count;
+    int count = 0;
+    public void dfs(TreeNode root, int target, HashMap<Long, Integer> map, long current) {
+        if(root == null) return;
+        current += root.val;
+        long needed = current - target;
+        if(map.containsKey(needed)) {
+            count += map.get(needed);
+        }
+        map.put(current, map.getOrDefault(current, 0) + 1);
+        dfs(root.left, target, map, current);
+        dfs(root.right, target, map, current);
+        map.put(current, map.getOrDefault(current, 0) - 1);
     }
     public int pathSum(TreeNode root, int targetSum) {
-        int total = 0;
-        if(root == null) return 0;
-        total += validPaths(root, targetSum);
-        total += pathSum(root.left, targetSum);
-        total += pathSum(root.right, targetSum);
-        return total;
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1);
+        long current = 0;
+        dfs(root, targetSum, map, current);
+        return count;
     }
 }

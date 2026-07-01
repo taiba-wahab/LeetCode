a22f2c5da2994a327aa9fe604437b0b49c12af63
taiba-wahab <taibawahab5@gmail.com>
2026-07-01 13:37:20 +0530

    Time: 0 ms (100%), Space: 46.7 MB (78.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,29 @@
 class Solution {
     public void helper(int[][] image, int sr, int sc, int color, int m, int n, int val) {
-        if(image[sr][sc] != color) {
-            image[sr][sc] = color;
-            // up
+        image[sr][sc] = color;
+        // up
         if(sr - 1 >= 0 && image[sr - 1][sc] == val) {
             helper(image, sr - 1, sc, color, m, n, val);
-            }
+        }
         // down
         if(sr + 1 < m && image[sr + 1][sc] == val) {
             helper(image, sr + 1, sc, color, m, n, val);
-            }
+        }
         // right
         if(sc + 1 < n && image[sr][sc + 1] == val) {
             helper(image, sr, sc + 1, color, m, n, val);
-            }
+        }
         // left 
         if(sc - 1 >= 0 && image[sr][sc - 1] == val) {
             helper(image, sr, sc - 1, color, m, n, val);
-            }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
        int m = image.length;
        int n = image[0].length;
        int original = image[sr][sc];
-       helper(image, sr, sc, color,  m, n, original);
+       if(original != color)
+        helper(image, sr, sc, color,  m, n, original);
        return image;
     }
 }

3bd26f0c4f8c6eae73abecc082f7f097afb5cdb3
taiba-wahab <taibawahab5@gmail.com>
2026-07-04 21:20:57 +0530

    Time: 0 ms (100%), Space: 47.3 MB (58.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,31 +1,19 @@
 class Solution {
-    public void dfs(int node, boolean[] visited, List<List<Integer>> graph) {
+    public void dfs(int node, boolean[] visited, int[][] isConnected) {
         visited[node] = true;
-        for(int neighbor : graph.get(node)) {
-            if(!visited[neighbor]) {
-                dfs(neighbor, visited, graph);
+        for(int neighbour = 0; neighbour < isConnected.length; neighbour++) {
+            if(isConnected[node][neighbour] == 1 && !visited[neighbour]) {
+                dfs(neighbour, visited, isConnected);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int provinces = 0;
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < isConnected.length; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for(int i = 0; i < isConnected.length; i++) {
-            for(int j = i + 1; j < isConnected[0].length; j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-            } 
-        }
-        boolean[] visited = new boolean[graph.size()];
+        boolean[] visited = new boolean[isConnected.length];
 
-        for(int i = 0; i < graph.size(); i++) {
+        for(int i = 0; i < isConnected.length; i++) {
             if(!visited[i]) {
-                dfs(i, visited, graph);
+                dfs(i, visited, isConnected);
                 provinces++;
             }
         }

943b2401ee97bbf8f998563542c332b6050359c0
taiba-wahab <taibawahab5@gmail.com>
2026-07-07 15:48:05 +0530

    Time: 1 ms (99.85%), Space: 42.4 MB (91.33%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,13 +1,23 @@
 class Solution {
     public int findX(int n) {
-        String num = String.valueOf(n);
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < num.length(); i++) {
-            if(num.charAt(i) != '0') {
-                sb.append(num.charAt(i));
+        int num = 0;
+        while(n != 0) {
+            int rem = n % 10;
+            if(rem != 0) {
+                num = 10 * num + rem;
             }
+            n /= 10;
         }
-        return Integer.parseInt(sb.toString());
+        return num;
+    }
+    public int reverse(int n) {
+        int rev = 0;
+        while(n != 0) {
+            int rem = n % 10;
+            rev = 10 * rev + rem;
+            n /= 10;
+        }
+        return rev;
     }
     public int sum(int n) {
         int s = 0;
@@ -20,7 +30,8 @@ class Solution {
     }
     public long sumAndMultiply(int n) {
         if(n == 0) return 0;
-        int x = findX(n);
+        int revX = findX(n);
+        int x = reverse(revX);
         int s = sum(x);
         return (long) x * s;
     }

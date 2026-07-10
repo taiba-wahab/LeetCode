5953a3022fd5c28ecd3e1c5d6d951d76fcb0bac9
taiba-wahab <taibawahab5@gmail.com>
2026-07-10 23:57:44 +0530

    Time: 0 ms (100%), Space: 45.4 MB (76.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,16 +14,16 @@
  * }
  */
 class Solution {
-    public int balanceFactor(TreeNode root) {
+    public int balanced(TreeNode root) {
         if(root == null) return 0;
-        int left = balanceFactor(root.left);
+        int left = balanced(root.left);
         if(left == -1) return -1;
-        int right = balanceFactor(root.right);
+        int right = balanced(root.right);
         if(right == -1) return -1;
         if(Math.abs(left - right) > 1) return -1;
         return Math.max(left, right) + 1;
     }
     public boolean isBalanced(TreeNode root) {
-        return balanceFactor(root) != -1;
+        return balanced(root) != -1;
     }
 }

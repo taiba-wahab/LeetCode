9a45a395796724551fa12e39211b77cce831ee09
taiba-wahab <taibawahab5@gmail.com>
2026-07-15 09:52:53 +0530

    Time: 1 ms (99.61%), Space: 45.5 MB (7.76%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,45 +13,34 @@
  *     }
  * }
  */
- class Pair {
-    long index;
-    TreeNode node;
-    Pair(TreeNode node, long index) {
-        this.node = node;
-        this.index = index;
-    }
- }
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        long maxWidth = 0L;
-        Pair temp = new Pair(root, 1);
-        q.offer(temp);
-        while(!q.isEmpty()) {
-            long first = 0, last = 0;
-            int size = q.size();
-            for(int i = 0; i < size; i++) {
-                Pair p = q.poll();
-                if(i == 0) {
-                    first = p.index;
-                }
-                if(i == size - 1) {
-                    last = p.index;
-                }
-                if(p.node != null) {
-                    if(p.node.left != null) {
-                        Pair newPair = new Pair(p.node.left, 2 * p.index + 1);
-                        q.offer(newPair);
-                    }
-                    if(p.node.right != null) {
-                        Pair newPair = new Pair(p.node.right, 2 * p.index + 2);
-                        q.offer(newPair);
-                    }
-                }
-            }
-            long width = last - first + 1;
-            maxWidth = Math.max(width, maxWidth);
+    class Pair {
+        int num;
+        TreeNode node;
+        Pair(TreeNode node, int num) {
+            this.num = num;
+            this.node = node;
         }
-        return (int)maxWidth;
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        int ans = 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+        while(!q.isEmpty()) {
+            int size = q.size();
+            int minOfLevel = q.peek().num;
+            int first = 0, last = 0;
+            for(int i = 0 ; i < size; i++) {
+                int currIdx = q.peek().num - minOfLevel;
+                TreeNode node = q.peek().node;
+                q.poll();
+                if(i == 0) first = currIdx;
+                if(i == size - 1) last = currIdx;
+                if(node.left != null) q.offer(new Pair(node.left, 2 * currIdx + 1));
+                if(node.right != null) q.offer(new Pair(node.right, 2 * currIdx + 2));
+            }
+            ans = Math.max(ans, last - first + 1);
+        }
+        return ans;
     }
 }

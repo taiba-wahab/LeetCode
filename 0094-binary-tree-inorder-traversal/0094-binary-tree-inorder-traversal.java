de77e6fa4f619b144a57dcb3b1054e2c308e648a
taiba-wahab <taibawahab5@gmail.com>
2026-07-23 10:53:10 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.69%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,39 +13,30 @@
  *     }
  * }
  */
- // MORRIS INORDER TRAVERSAL 
- // Time Complexity : O(2n) --> as every node is visited twice
- // Space Complexity : O(1) --> no extra memory required
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
         TreeNode curr = root;
         TreeNode pred = null;
         while(curr != null) {
-            // if no left
             if(curr.left == null) {
                 ans.add(curr.val);
                 curr = curr.right;
             }
-            // if left exists --> find predecessor and make threads to curr
             else {
-                // find predecessor
-                pred = curr.left; // intially make curr.left as pred of curr
+                pred = curr.left;
                 while(pred.right != null && pred.right != curr) {
-                    // pred.right != null is for
-                    //checking for the situation that its the first time we are visitng the node not second time       
                     pred = pred.right;
-                } 
-                if(pred.right == null) {// left has not been visited before
+                }
+                if(pred.right == null) {
                     pred.right = curr;
                     curr = curr.left;
                 }
                 else {
-                    // left has been visited 
-                    ans.add(curr.val); // so visit current node
-                    pred.right = null; // dissolve thread
-                    curr = curr.right; // move right
+                    ans.add(curr.val);
+                    pred.right = null;
+                    curr = curr.right;
                 }
             }
         }

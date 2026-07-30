5d3c56e89169e0e6d239fbc30e13ee6ecf973f59
taiba-wahab <taibawahab5@gmail.com>
2026-07-30 22:33:25 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,17 +1,6 @@
 class Solution {
     public int minimumPushes(String word) {
         int rounds = word.length() / 8;
-        int i = 1;
-        int answer = 0;
-        if(word.length() % 8 != 0) {
-            answer = answer + ((rounds + 1) * (word.length() % 8));
-        }
-        while(rounds != 0) {
-            answer += 8 * i;
-            i++;
-            rounds--;
-        } 
-        
-        return answer;
+        return (rounds * (rounds + 1) / 2) * 8 + (word.length() % 8) * (rounds + 1);
     }
 }

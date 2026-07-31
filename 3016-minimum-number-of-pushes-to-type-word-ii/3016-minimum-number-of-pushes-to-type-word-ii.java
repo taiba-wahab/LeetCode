09670a2f707f778eb38b41fe51923c220e76be0c
taiba-wahab <taibawahab5@gmail.com>
2026-07-31 14:58:46 +0530

    Time: 9 ms (94.22%), Space: 48.1 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -3,19 +3,19 @@ class Solution {
         int answer = 0;
         int[] freq = new int[26];
         for(int i = 0; i < word.length(); i++) {
-            freq[word.charAt(i) - 97]++;
+            freq[word.charAt(i) - 'a']++;
         }
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < 26; i++) {
             if(freq[i] != 0) list.add(freq[i]);
         }
         Collections.sort(list, Collections.reverseOrder());
-        int factor = 1;
+        int pushCost = 1;
         for(int i = 0; i < list.size(); i++) {
             if(i % 8 == 0 && i != 0) {
-                factor++;
+                pushCost++;
             }
-            answer += list.get(i) * factor;
+            answer += list.get(i) * pushCost;
         }
         return answer;
     }

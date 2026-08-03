9821faef42472c884d3392fe24c05c26a53df9ee
taiba-wahab <taibawahab5@gmail.com>
2026-08-03 10:19:45 +0530

    Time: 60 ms (79.68%), Space: 107.7 MB (32.92%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prev = stack.pop();
+                answer[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}

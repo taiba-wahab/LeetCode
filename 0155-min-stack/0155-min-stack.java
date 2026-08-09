7036cf8372fc993067b78716ea9f3c5c35d12c7f
taiba-wahab <taibawahab5@gmail.com>
2026-08-09 23:28:48 +0530

    Time: 30 ms (89.85%), Space: 102.7 MB (15.64%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,57 +1,50 @@
-class Pair {
-    int val;
-    int curr_min;
-    Pair(int val, int curr_min) {
-        this.val = val;
-        this.curr_min = curr_min;
-    }
-}
 class MinStack {
-    Stack<Pair> stack;
+    Stack<Long> stack;
+    long min = Long.MAX_VALUE;
+
     public MinStack() {
         stack = new Stack<>();
-
     }
     
     public void push(int val) {
-        int min_value = val;
-        if(!stack.empty()) {
-            min_value = stack.peek().curr_min < min_value ? stack.peek().curr_min : min_value;
+        long x = val;
+
+        if(stack.isEmpty()) {
+            min = x;
+            stack.push(x);
         }
-        Pair new_pair = new Pair(val, min_value);
-        stack.push(new_pair);
+        else {
+            if(x > min) {
+                stack.push(x);
+            }
+            else {
+                stack.push(2 * x - min);
+                min = x;
+            }
+        }
     }
     
     public void pop() {
-        if(!stack.empty()) {
-            stack.pop();
+        if(stack.isEmpty()) return;
+
+        long x = stack.peek();
+        stack.pop();
+
+        if(x < min) {
+            min = 2 * min - x;
         }
     }
     
     public int top() {
-        if(!stack.empty()) {
-            return stack.peek().val;
-        }
-        else {
-            return -1;
-        }
+        if(stack.isEmpty()) return -1;
+
+        long x = stack.peek();
+
+        if(min < x) return (int)x;
+        return (int)min;
     }
     
     public int getMin() {
-        if(!stack.empty()) {
-            return stack.peek().curr_min;
-        }
-        else {
-            return -1;
-        }
+        return (int)min;
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */

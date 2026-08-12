38ba3991e135b0b4b4b9184e29481e66cdd6f2a7
taiba-wahab <taibawahab5@gmail.com>
2026-08-12 11:44:41 +0530

    Time: 2 ms (36.64%), Space: 46.3 MB (36.79%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -20,48 +20,26 @@ class Node {
     }
 };
 */
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
 
 class Solution {
     public Node connect(Node root) {
+        Queue<Node> q = new LinkedList<>();
         if(root == null) return root;
-            Queue<Node> q = new LinkedList<>();
-            q.offer(root);
-            while(!q.isEmpty()) {
-                int levelSize = q.size();
-                for(int i = 0; i < levelSize; i++) {
-                    Node node = q.poll();
-                    if(i == levelSize - 1) {
-                        node.next = null;
-                    }
-                    else {
-                        node.next = q.peek();
-                    }
-                    if(node.left != null) q.offer(node.left);
-                    if(node.right != null) q.offer(node.right);
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++) {
+                Node temp = q.poll();
+                if(i == levelSize - 1) {
+                    temp.next = null;
                 }
+                else {
+                    temp.next = q.peek();
+                }
+                if(temp.left != null) q.offer(temp.left);
+                if(temp.right != null) q.offer(temp.right);
             }
+        } 
         return root;
     }
 }

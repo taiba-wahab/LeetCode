770d990bdb5dcbc9078723b5d2dd779ec82ed606
taiba-wahab <taibawahab5@gmail.com>
2026-08-14 23:47:33 +0530

    Time: 2 ms (97.99%), Space: 46.5 MB (98.09%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> st = new ArrayList<>();
+        for(int i = 0; i < asteroids.length; i++) {
+            if(asteroids[i] > 0) st.add(asteroids[i]);
+            else {
+                while(st.size() != 0 && st.get(st.size() - 1) > 0 && st.get(st.size() - 1) < Math.abs(asteroids[i])) st.remove(st.size() - 1);
+                if(st.size() != 0 && st.get(st.size() - 1) == Math.abs(asteroids[i])) st.remove(st.size() - 1);
+                else if(st.size() == 0 || st.get(st.size() - 1) < 0) st.add(asteroids[i]);
+            }
+        }
+        int[] answer = new int[st.size()];
+        for(int i = 0; i < st.size(); i++) {
+            answer[i] = st.get(i);
+        }
+        return answer;
+    }
+}

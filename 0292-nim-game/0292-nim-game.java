a96e576e4698dce8f2f4abb7bca5957c938d28fc
taiba-wahab <taibawahab5@gmail.com>
2026-08-17 10:59:34 +0530

    Time: 0 ms (100%), Space: 41.4 MB (99.26%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n <= 3) return true;
-        if(n % 4 == 0) return false;
-        else return true;
+        return (n % 4 != 0);
     }
 }

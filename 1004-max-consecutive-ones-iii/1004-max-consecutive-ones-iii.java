05bfac5414f569f3a223fc9381a9f0faed0fc295
taiba-wahab <taibawahab5@gmail.com>
2026-09-01 14:52:14 +0530

    Time: 3 ms (95.99%), Space: 52.1 MB (54.78%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,19 +3,15 @@ class Solution {
         int ans = Integer.MIN_VALUE;
         int left = 0, right = 0, flips = 0, count = 0;
         while(right < nums.length) {
-            if(nums[right] == 1) {
-                count++;
-            }
-            if(nums[right] == 0 && flips <= k) {
+            if(nums[right] == 0) {
                 flips++;
                 count++;
             }
             while(flips > k) {
                 if(nums[left] == 0) flips--;
                 left++;
-                count = right - left + 1;
             }
-            ans = Math.max(ans, count);
+            ans = Math.max(ans, right - left + 1);
             right++;
         }
         return ans;
